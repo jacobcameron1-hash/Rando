7c93469d2720bac18f1c970bcc6517ebee80f810
jacobcameron1-hash <jacobcameron1@gmail.com>
2026-03-22 09:43:26 -0400

    Remove Drizzle config type check for build

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,12 @@
-import 'dotenv/config';
-import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
 
-export default defineConfig({
+config({ path: '.env.local' });
+
+export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
   },
-});
+};
